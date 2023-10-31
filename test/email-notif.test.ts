c608e3ff11a2bcea9b70db5ce858eaa44b1059a9
utkarsh adlakha <utkarshadlakha1702@gmail.com>
2023-10-31 16:01:57 +0530

    Initial commit

---
+++ test/email-notif.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EmailNotif from '../lib/email-notif-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/email-notif-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EmailNotif.EmailNotifStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
